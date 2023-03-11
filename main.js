8a768b04a22763b03e09e4c33a369f4e01d42e61
unknown <sayemhaque>
2023-03-11 22:21:45 +0600

    added function more simple way

--- main.js
+++ main.js
@@ -1,28 +1,51 @@
-const getValueFromInput = () => {
-   const userNameField  = document.getElementById("name");
-   const userName = userNameField.value;
-   return userName;
+const getValueFromInput = (element) => {
+    const field = document.getElementById(element);
+    const value = field.value;
+    return value;
 }
 
 const addData = () => {
-  const userName =   getValueFromInput()
-  localStorage.setItem("username" , userName)
-  window.location.reload();
-}
-const removeData = () => {
-    const userName = getValueFromInput()
-    localStorage.removeItem("username" , userName)
+    const userName = getValueFromInput("name")
+    const email = getValueFromInput("email")
+
+    if (userName) localStorage.setItem("username", userName)
+    if (email) localStorage.setItem("email", email)
     window.location.reload();
-
 }
 
+
+const removeData = () => {
+    const userName = getValueFromInput("name")
+    const email = getValueFromInput("email");
+
+    document.getElementById("removeName").addEventListener("click", () => {
+        localStorage.removeItem("username", userName)
+        window.location.reload();
+    })
+
+    document.getElementById("removeEmail").addEventListener("click", () => {
+        localStorage.removeItem("email", email)
+        window.location.reload();
+    })
+}
+
+
+removeData()
+
+
+const removeAll = () => {
+   localStorage.clear();
+   window.location.reload();
+}
 
 const displayData = () => {
     const parentDiv = document.getElementById("parentDiv");
-  const userName =  localStorage.getItem("username")
-  const email = localStorage.getItem("email");
-  console.log(userName)
-    parentDiv.innerHTML = `${userName ? `<li>Name : ${userName}</li>` : ""}`
+    const userName = localStorage.getItem("username")
+    const email = localStorage.getItem("email");
+    console.log(userName)
+    parentDiv.innerHTML = `${userName ? `<li>Name : ${userName}</li>` : ""}
+    ${email ? `<li>Email : ${email}</li>` : ""}
+    `
 }
 
 displayData()
